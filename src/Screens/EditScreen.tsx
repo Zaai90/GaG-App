ededbfe40570c1adc10e37ece7ca5aeca37c20c2
Daniel T <90195897+DTSD21@users.noreply.github.com>
2022-09-26 09:16:57 +0200

    add EditScreen component

---
+++ src/Screens/EditScreen.tsx
@@ -0,0 +1,16 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import { Text, View } from "react-native";
+import { RootStackParamList } from "../navigation/types";
+
+type Props = NativeStackScreenProps<RootStackParamList, "EditScreen">;
+
+const EditScreen = ({ navigation }: Props) => {
+  return (
+    <View>
+      <Text>Edit Screen</Text>
+    </View>
+  );
+};
+
+export default EditScreen;
