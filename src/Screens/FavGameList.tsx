36784e6f7895818adac2003de3ae120a2f8950d7
LucasB <lucas_benjaminson@hotmail.com>
2022-09-26 11:00:03 +0200

    Add Buttons on FavGameList and Settings

--- src/Screens/FavGameList.tsx
+++ src/Screens/FavGameList.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { RootStackParamList } from "../navigation/types";
 
 type Props = NativeStackScreenProps<RootStackParamList, "FavGameListScreen">;
@@ -9,6 +9,18 @@ const FavGameListScreen = ({ navigation }: Props) => {
   return (
     <View>
       <Text>Show all fav games</Text>
+      <Button
+        title="Settings"
+        onPress={() => {
+          navigation.navigate("Settings");
+        }}
+      />
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
     </View>
   );
 };
