3f978ff8e74b9e94a057f04ea25695d74dfe2cb0
Zaai90 <erik.grune@gmail.com>
2022-09-27 16:07:05 +0200

    change name of type

--- src/components/GotoButton.tsx
+++ src/components/GotoButton.tsx
@@ -1,13 +1,13 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { Button, StyleSheet } from 'react-native';
-import { OmitGotoStack, RootStackParamList } from '../navigation/types';
+import { OmitGotoParamList, RootStackParamList } from '../navigation/types';
 
 
 type Props =
     {
-        navigator: NativeStackScreenProps<RootStackParamList, keyof OmitGotoStack>;
-        to: keyof OmitGotoStack;
+        navigator: NativeStackScreenProps<RootStackParamList, keyof OmitGotoParamList>;
+        to: keyof OmitGotoParamList;
         title: string
     }
 
