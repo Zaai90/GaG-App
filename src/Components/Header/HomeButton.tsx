bf5c96f7ab06146daf5ad555b744daa54ef1653c
Frans Billqvist <frans.billquist@gmail.com>
2022-09-28 23:44:21 +0200

    Fixed import path solved problem

--- src/Components/Header/HomeButton.tsx
+++ src/Components/Header/HomeButton.tsx
@@ -1,18 +1,20 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
-import { Pressable } from "react-native";
-import { FontAwesome } from '@expo/vector-icons'; 
+import { Pressable, Text } from "react-native";
+import { Octicons, Ionicons } from '@expo/vector-icons'; 
 
 const HomeButton = () => {
     const navigation = useNavigation();
     const route = useRoute();
-  if (route.name != "home"){
-    return (<Pressable onPress={
-        () => navigation.navigate("Home")}>
-      <FontAwesome name="home" size={24} color="black" />
-      </Pressable>)
-      }
-      else return null;
-  }
+  if (route.name !== "Home"){
+    return (
+      <>
+      <Pressable onPress={() => navigation.navigate("Home")}>
+        <Ionicons name="md-home" size={21} color="black" />
+      </Pressable>
+      </>
+      );
+    } else return null;
+  };
 
   export default HomeButton;
