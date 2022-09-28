14a73f3d26909c1c9e6f7701816a7470f98b3e1a
Zaai90 <erik.grune@gmail.com>
2022-09-28 11:56:47 +0200

    make styling overridable

--- src/Components/GotoButton.tsx
+++ src/Components/GotoButton.tsx
@@ -1,28 +1,30 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleProp, StyleSheet, Text, TextStyle, ViewStyle } from "react-native";
 import { OmitGotoParamList, RootStackParamList } from "../navigation/types";
 
-interface StyleProps {
-  backgroundColor?: string;
-  textColor?: string;
-}
 
 type Props = {
   navigator: NativeStackScreenProps<RootStackParamList, keyof OmitGotoParamList>;
   to: keyof OmitGotoParamList;
   title: string;
-  style?: StyleProps;
+  style?: ButtonStyle;
 }
+
+type ButtonStyle = {
+  button?: StyleProp<ViewStyle>;
+  text?: StyleProp<TextStyle>;
+}
+
 
 const GotoButton = ({ navigator, title, to, style }: Props) => {
   return (
-    <Pressable style={styles(style).button}
+    <Pressable style={[basicStyle.button, style?.button]}
       onPress={() => {
         navigator.navigation.navigate(to);
       }}
     >
-      <Text style={styles(style).text}>{title}</Text>
+      <Text style={[basicStyle.text, style?.text]}>{title}</Text>
 
     </Pressable>
   );
@@ -31,7 +33,7 @@ const GotoButton = ({ navigator, title, to, style }: Props) => {
 export default GotoButton;
 
 
-const styles = (style?: StyleProps) => StyleSheet.create({
+const basicStyle = StyleSheet.create({
 
   button: {
     marginVertical: 5,
@@ -41,13 +43,13 @@ const styles = (style?: StyleProps) => StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: style?.backgroundColor || '#876796',
+    backgroundColor: '#876796',
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
     fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: style?.textColor || "white",
+    color: "white",
   },
 });
