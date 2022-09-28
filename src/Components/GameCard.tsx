11924276c5785afdd639c6270b38427b26a04ff6
LucasB <lucas_benjaminson@hotmail.com>
2022-09-28 21:51:13 +0200

    The endless grind of styling

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,5 +1,6 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { Game } from "../Data/game";
 
 interface Props {
@@ -9,13 +10,27 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <View style={style.container}>
-      <View style={{ display: "flex", flexDirection: "row" }}>
+      <View>
         <Image resizeMode='stretch' source={{ uri: game.imgUrl }} style={style.image} />
-        <View style={{ flexShrink: 1 }}>
-          <Text style={style.titleText}>{game.title}</Text>
-          <Text style={style.text}>Genre: {game.genre}</Text>
-          <Text style={style.text}>{game.developer}</Text>
-          <Text style={style.text}>{game.score}</Text>
+      </View>
+
+      <View>
+        <View style={{ flexDirection: "row" }}>
+          <View style={style.textContainer}>
+            <Text style={style.titleText}>{game.title}</Text>
+            <Text style={style.text}>Genre: {game.genre}</Text>
+            <Text style={style.text}>Developer: {game.developer}</Text>
+            <Text style={style.text}>{game.score}</Text>
+          </View>
+
+          <View style={{ backgroundColor: "green" }}>
+            <MaterialCommunityIcons name='cards-heart-outline' size={24} color='purple' />
+            <MaterialCommunityIcons name='pencil' size={24} color='purple' />
+          </View>
+        </View>
+
+        <View style={{ justifyContent: "flex-end", flexGrow: 1, backgroundColor: "yellow" }}>
+          <Button title='Delete game' />
         </View>
       </View>
     </View>
@@ -24,6 +39,8 @@ const GameCard = ({ game }: Props) => {
 
 const style = StyleSheet.create({
   container: {
+    display: "flex",
+    flexDirection: "row",
     backgroundColor: "#EBD4C2",
     padding: 10,
     minWidth: "95%",
@@ -35,6 +52,7 @@ const style = StyleSheet.create({
     shadowOffset: { width: -10, height: 10 },
     shadowOpacity: 0.2,
     shadowRadius: 5,
+    height: 185,
   },
   text: {
     fontFamily: "Roboto",
@@ -44,10 +62,20 @@ const style = StyleSheet.create({
     fontFamily: "Roboto",
     fontSize: 20,
     fontWeight: "700",
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 2, height: 3 },
+    textShadowRadius: 5,
+    textShadowOpacity: 0.2,
   },
   image: {
-    height: "100%",
+    height: 100,
     width: 90,
+  },
+  textContainer: {
+    flexGrow: 1,
+    paddingLeft: 10,
+    backgroundColor: "red",
+    flexWrap: "wrap",
   },
 });
 
