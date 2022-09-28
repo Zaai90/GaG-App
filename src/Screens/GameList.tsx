46c8238fbc7910d347967cbc5923ab752fe8ddf9
Frans Billqvist <frans.billquist@gmail.com>
2022-09-28 11:37:44 +0200

    Major container work done, still missing button style

--- src/Screens/GameList.tsx
+++ src/Screens/GameList.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, StyleSheet, ScrollView } from "react-native";
 import GameCard from "../Components/GameCard";
 import GotoButton from "../Components/GotoButton";
 import mockData from "../Data/mockData";
@@ -10,17 +10,49 @@ type Props = NativeStackScreenProps<RootStackParamList, "GameList">;
 
 const GameList = (navigator: Props) => (
   <View>
-    <Text>Game list screen </Text>
+  <ScrollView contentContainerStyle={styles.container}>
+    {/* <View style={styles.container}> */}
 
     {mockData.map((game) => (
-      <GameCard key={game.id} game={game} />
-    ))}
-
+      <View key={game.id} style={styles.cardContainer}>
+        <GameCard game={game} />
+      </View>
+      ))}
+  </ScrollView>
+  <View style={styles.ButtonBar}>
+    <View style={styles.AddButton}>
     <GotoButton title='Add game' to='Add' navigator={navigator} />
-    <Pressable>
-      <Text>Edit</Text>
-    </Pressable>
+    </View>
+    </View>
   </View>
 );
 
 export default GameList;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 15,
+    paddingHorizontal: 4,
+    paddingLeft: 4,
+    paddingBottom: 70,
+  },
+  cardContainer: {
+    marginBottom: 5,
+  },
+  ButtonBar: {
+    flex: 1,
+    position: "absolute",
+    alignItems: "center",
+    backgroundColor: "#876796",
+    width: "100%",
+    bottom: 0,
+    paddingVertical:10,
+  },
+  AddButton: {
+    backgroundColor: "#876796",
+    borderRadius: 10,
+  },
+});
