e4fe5498d1f8cdd6834605db56dc9457926d2a5c
Frans Billqvist <frans.billquist@gmail.com>
2022-09-30 16:04:15 +0200

    Added interface and tearnery for button

--- src/Components/Speech.tsx
+++ src/Components/Speech.tsx
@@ -2,17 +2,23 @@ import * as Speech from "expo-speech";
 import React from "react";
 import { Button, View } from "react-native";
 import { useGameContext } from "../Context/GameContext";
+import { Game } from "../Data/game";
 
-function TextToSpeech(gameId: string) {
-  const { getGameById, games } = useGameContext();
-  const game = getGameById(gameId);
+interface Props {
+  gameId: string;
+}
+
+function TextToSpeech({gameId}: Props) {
+  const { getGameById} = useGameContext();
+  const game: Game = getGameById(gameId);
 
   const speak = () => {
     const genreToSay = game.genre;
     const titleToSay = game.title;
     const developerToSay = game.developer;
     const scoreToSay = game.score;
-    const gameToSay = titleToSay + genreToSay + developerToSay + scoreToSay;
+    const gameToSay = "You should play " + titleToSay +" genre is "+ genreToSay + " Developed by" 
+    + developerToSay + " the game is rated " + scoreToSay + " out of 5";
     Speech.speak(gameToSay);
   };
 
