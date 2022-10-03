6a3fb2608944e3b29c080868252dad4f5414609f
Zaai90 <erik.grune@gmail.com>
2022-10-03 22:58:43 +0200

    change name of component and add functionality to get systems brightness

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -2,11 +2,21 @@ import Slider, { SliderProps } from "@react-native-community/slider";
 import * as Brightness from "expo-brightness";
 import React, { useEffect, useState } from "react";
 
-const SliderExample = (props: SliderProps) => {
-  const [brightness, setBrightness] = useState(0);
+const BrightnessSlider = (props: SliderProps) => {
+  const [brightness, setBrightness] = useState(0.5);
   const [granted, setGranted] = useState(false);
 
-  //   const [slideStartingValue, setSlideStartingValue] = useState(0);
+  const getBrightnessFromSystemAsync = async () => {
+    const { status } = await Brightness.requestPermissionsAsync();
+    setGranted(status === "granted");
+    if (status === "granted") {
+      await Brightness.useSystemBrightnessAsync();
+      const value = await Brightness.getSystemBrightnessAsync();
+      console.log("Set to " + value);
+      setBrightness(value);
+    }
+  };
+
   useEffect(() => {
     (async () => {
       const { status } = await Brightness.requestPermissionsAsync();
@@ -18,32 +28,36 @@ const SliderExample = (props: SliderProps) => {
   useEffect(() => {
     if (granted === true) {
       (async () => {
-        const brigth = await Brightness.getBrightnessAsync();
-        setBrightness(brigth);
-      })();
+        console.log("Getting Systems brightness");
+        await getBrightnessFromSystemAsync();
+      }
+      )();
     }
-    console.log(brightness);
   }, [granted]);
 
   useEffect(() => {
     (async () => {
-      Brightness.setBrightnessAsync(brightness);
+      console.log("Brightness set to " + brightness);
+      if (granted) {
+        await Brightness.setSystemBrightnessAsync(brightness)
+      }
     })();
-
-    console.log(brightness);
   }, [brightness]);
+
+
 
   return (
     <Slider
       step={0.05}
       value={brightness}
+      maximumValue={0.9}
       {...props}
       onValueChange={setBrightness}
       style={{ width: "75%", height: 40 }}
-      // minimumTrackTintColor='#0FE2FF'
-      // maximumTrackTintColor='#3837BA'
+    // minimumTrackTintColor='#0FE2FF'
+    // maximumTrackTintColor='#3837BA'
     />
   );
 };
 
-export default SliderExample;
+export default BrightnessSlider;
