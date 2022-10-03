2576ccbc9ccf88b5bbd5ae37801809eb755522bc
Frans Billqvist <frans.billquist@gmail.com>
2022-10-03 11:38:05 +0200

    Added req funcality

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -4,14 +4,22 @@ import { View } from "react-native";
 import GotoButton from "../Components/GotoButton";
 import HapticButton from "../Components/Haptics";
 import { RootStackParamList } from "../navigation/types";
+import { useGameContext } from "../Context/GameContext";
+
+
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 const Home = (navigator: Props) => {
+  const { getFavGames } = useGameContext();
+  const FavGames = getFavGames();
   return (
     <View>
       <HapticButton title='GaG me!😘' to='PlayThis' navigator={navigator} hapticType='Heavy' />
-      <HapticButton title='GaG a Fav😊' to='PlayThisFav' navigator={navigator} hapticType='Heavy' />
+      {FavGames.length > 0 ? ( 
+       <HapticButton title='GaG a Fav😊' to='PlayThisFav' navigator={navigator} hapticType='Heavy' />) : null
+      }
+      
       <GotoButton title='List of your games' to='GameList' navigator={navigator} />
       <GotoButton title='List of your favorite games' to='FavGameList' navigator={navigator} />
     </View>
