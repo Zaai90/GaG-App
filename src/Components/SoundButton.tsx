af34ef3da595ce13d7782ea563911b174a14a11e
Daniel T <90195897+DTSD21@users.noreply.github.com>
2022-10-04 15:02:34 +0200

    add basicStyle to soundbutton

--- src/Components/SoundButton.tsx
+++ src/Components/SoundButton.tsx
@@ -1,14 +1,20 @@
 import { Audio } from "expo-av";
 import React from "react";
-import { Pressable, Text } from "react-native";
+import { Pressable, StyleProp, StyleSheet, Text, TextStyle, ViewStyle } from "react-native";
 import DeathSoundEffect from "../../assets/DeathSoundEffect.wav";
 
 interface Props {
   onPress: () => void;
   title: string;
+  style?: ButtonStyle;
 }
 
-const SoundButton = ({ title, onPress }: Props) => {
+type ButtonStyle = {
+  button?: StyleProp<ViewStyle>;
+  text?: StyleProp<TextStyle>;
+};
+
+const SoundButton = ({ title, onPress, style }: Props) => {
   const [sound, setSound] = React.useState<Audio.Sound | null>(null);
 
   async function playSound() {
@@ -23,18 +29,43 @@ const SoundButton = ({ title, onPress }: Props) => {
   React.useEffect(() => {
     return sound
       ? () => {
-        console.log("Unloading Sound");
-        sound.unloadAsync();
-      }
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
       : undefined;
   }, [sound]);
 
-  return <Pressable onPress={() => {
-    playSound();
-    onPress();
-    sound?.stopAsync();
-
-  }}><Text>{title}</Text></Pressable >
+  return (
+    <Pressable
+      style={[basicStyle.button, style?.button]}
+      onPress={() => {
+        playSound();
+        onPress();
+        sound?.stopAsync();
+      }}
+    >
+      <Text>{title}</Text>
+    </Pressable>
+  );
 };
-
 export default SoundButton;
+
+const basicStyle = StyleSheet.create({
+  button: {
+    marginVertical: 5,
+    paddingVertical: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#876796",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+});
